eee135459914b4de54840a7d4852c2135c3bb5f7
Antykors <43018931+Antykors@users.noreply.github.com>
2023-03-08 20:40:47 +0100

    Update user_sim.sql

--- resources/[3.Standalone]/[téléphone]/esx-sim-npwd/user_sim.sql
+++ resources/[3.Standalone]/[téléphone]/esx-sim-npwd/user_sim.sql
@@ -1,11 +1,7 @@
-USE `es_extended`;
-
 CREATE TABLE `user_sim` (
   `id` int NOT NULL AUTO_INCREMENT,
   `identifier` varchar(555) NOT NULL,
-  `number` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `label` varchar(555) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-ALTER TABLE `user_sim`
-  ADD PRIMARY KEY (`id`);
+  `number` varchar(20) NOT NULL,
+  `label` varchar(555) NOT NULL,
+  PRIMARY KEY (ID)
+) ENGINE=InnoDB;
