8b25aa45ce2e093916addf054315813a4869fdf5
Antykors <43018931+Antykors@users.noreply.github.com>
2023-03-12 19:05:52 +0100

    Delete resources/[5.Antykors]/[métiers]/d-farming directory

--- resources/[5.Antykors]/[métiers]/d-farming/locales/de.lua
+++
@@ -1,7 +0,0 @@
-Locales['de'] = {
-    ['needitem'] = "Du brauchst das Item %s um zu farmen",
-    ['itemdoenstexist'] = "Das Item zum farmen existiert nicht",
-    ['nospace'] = "So viel kannst du nicht halten.",
-    ['justjobcanfarm'] = "Nur die %s über Rang %s können hier minen",
-    ['success'] = "Du hast %sx %s erhalten",
-}
--- resources/[5.Antykors]/[métiers]/d-farming/locales/en.lua
+++
@@ -1,7 +0,0 @@
-Locales['en'] = {
-    ['needitem'] = "Vous avez besoin de %s pour commencer à récolter",
-    ['itemdoenstexist'] = "L'item à récolter n'éxiste pas en base de données",
-    ['nospace'] = "Vous n'avez plus assez de place.",
-    ['justjobcanfarm'] = "Seuls %s avec le rang %s ou supérieur peuvent l'utiliser",
-    ['success'] = "Succès ! Vous récoltez %sx %s",
-}
