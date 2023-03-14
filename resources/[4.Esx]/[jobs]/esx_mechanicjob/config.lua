2fff0b72f53733bffb8711bb6d133237eca3c147
Antykors <43018931+Antykors@users.noreply.github.com>
2023-03-14 15:14:06 +0100

    Add files via upload

--- resources/[4.Esx]/[jobs]/esx_mechanicjob/config.lua
+++ resources/[4.Esx]/[jobs]/esx_mechanicjob/config.lua
@@ -46,13 +46,13 @@ Config.Zones = {
 	},
 
 	VehicleSpawnPoint = {
-		Pos   = vector3(-366.354, -110.766, 37.696 ),
+		Pos   = vector3(-366.354, -110.766, -37.696 ),
 		Size  = { x = 1.5, y = 1.5, z = 1.0 },
 		Type  = -1
 	},
 
 	VehicleDeleter = {
-		Pos   = vector3(-386.899, -105.675, 37.683 ),
+		Pos   = vector3(-386.899, -105.675, -37.683 ),
 		Size  = { x = 3.0, y = 3.0, z = 1.0 },
 		Color = { r = 204, g = 204, b = 0 },
 		Type  = 1
